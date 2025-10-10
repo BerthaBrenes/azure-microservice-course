71ab9795ed8062d0e4f7b1094dfc84a9cc378051
bbrenesbrene <bbrenesbrene@microsoft.com>
2025-10-10 12:35:18 -0600

    fix error on controller

--- src/Wpm.Clinic/Controllers/ConsultationController.cs
+++ src/Wpm.Clinic/Controllers/ConsultationController.cs
@@ -6,7 +6,7 @@ namespace Wpm.Clinic.Controllers;
 [Route("api/[controller]")]
 public class ConsultationController(ClinicApplicationService applicationService, ILogger<ConsultationController> logger) : ControllerBase
 {
-    [HttpPost("/start")]
+    [HttpPost("start")]
     public async Task<IActionResult> Start(StartConsultationCommand command)
     {
         var result = await applicationService.Handle(command);
